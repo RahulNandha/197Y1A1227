8745cd2302809a167ee955091ddc86263c22412f
Rahul <rahulnandha36@gmail.com>
2021-11-01 22:05:21 +0530

    rahul

---
+++ triangle.cpp
@@ -0,0 +1,28 @@
+#include<iostream.h>
+std::
+void main()
+{
+    int a,b,c;
+    cout<<"enter 3 sides";cin>>a>>b>>c;
+    if(a+b>c&&b+c>a&&c+a>b)
+    {
+        if((a*a+b*b)==c*c||(b*b+c*c)==a*a||(c*c+a*a)==b*b)
+    {
+        cout<<"right angled";
+    }
+    else if((a==b&&b!=c)||(b==c&&c!=a)||(c==a&&a!=b
+    {
+        cout<<"isosceles";
+    }
+    else if (a==b&&b==c)
+    {
+        cout<<"equileteral";
+    }
+    else 
+    {
+        cout<<"any valid triangle";
+    }
+    cout<<"cannot form a valid triangle";
+    }
+    }
+}
