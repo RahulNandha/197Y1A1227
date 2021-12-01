43c12b33ee21fb62af6b7253d71b2d1b1162faf3
Rahul <rahulnandha36@gmail.com>
2021-12-01 19:42:51 +0530

    basic of pyton

--- natural.c
+++ natural.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    int,i,s=0,sum;
+    int i,s=0,sum;
     s=s+i;
     i++;
     if(i<=10);
-    go to sum;
+    sum;
     printf("sum of 1st natural numbers are %d",s);
 }
