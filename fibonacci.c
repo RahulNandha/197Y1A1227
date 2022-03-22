5376dac55fa73db9a373e6f513e1b3682516e327
Rahul <rahulnandha36@gmail.com>
2022-03-22 17:10:46 +0530

    rahul

--- fibonacci.c
+++ fibonacci.c
@@ -4,11 +4,12 @@ int main()
     int n1=0,n2=1,n3,i,nbr;
     printf("enter the no of elements:");
     scanf("\n %d %d",n1,n2);
-    for(i=2;i<nbr;i++)
+    for(i=2;i<nbr;++i)
     {
         n3=n1+n2;
         printf("%d",n3);
         n1=n2;
         n2=n3;
     }
+  return 0;
 }
